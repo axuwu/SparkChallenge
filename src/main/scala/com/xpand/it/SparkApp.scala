0dc9ee6f35f7ee8fa78912bb766c5f410b5f49cc
António Wu <81578829+30001546@users.noreply.github.com>
2022-09-01 16:46:13 +0100

    Part2 and modded Part1
    part2 exercise
    -df gapps to obtain all Apps with a "Rating" greater or equal to 4.0 sorted in descending order.
    -df gapps as .csv
    
    part1 modded

--- src/main/scala/com/xpand/it/SparkApp.scala
+++ src/main/scala/com/xpand/it/SparkApp.scala
@@ -23,24 +23,104 @@ object SparkApp {
       .getOrCreate()
 
     //imported csv
-    val googleUserReviews = spark.read.option("header", true).csv("googleplaystore_user_reviews.csv")
-    val googlePlayStore = spark.read.option("header", true).csv("googleplaystore.csv")
+    val googleUserReviews:DataFrame = spark.read.option("header", true).csv("googleplaystore_user_reviews.csv")
+    val googlePlayStore:DataFrame = spark.read.option("header", true).csv("googleplaystore.csv")
 
-    /**
-     * Part 1
-     */
     println("--------------------------------------------------Part 1--------------------------------------------------")
-    var df_1: DataFrame = googleUserReviews
-      .groupBy("App")//groups by App
-      .agg(avg(googleUserReviews("Sentiment_Polarity")).as("Average_Sentiment_Polarity")) //does the average of Polarity
-      .withColumn("Average_Sentiment_Polarity", col("Average_Sentiment_Polarity").cast(DoubleType)) //turns them as Double
-      .na.fill(0)// fills the NaN as 0.0
+    var df_1: DataFrame = partOne(googleUserReviews)
 
     df_1.printSchema()
     df_1.show()
 
+    println("--------------------------------------------------Part 2--------------------------------------------------")
+    var df_2 = partTwo(googlePlayStore, "§", "Ex2/", "best_apps.csv", spark)
+
+    df_2.printSchema()
+    df_2.show()
+
+
     println("Quitting....")
     spark.close()
+  }
+
+  //Part 1
+  def partOne(df:DataFrame): DataFrame = {
+    var df_1: DataFrame = df
+      .groupBy("App")//groups by App
+      .agg(avg(df("Sentiment_Polarity")).as("Average_Sentiment_Polarity")) //does the average of Polarity
+      .withColumn("Average_Sentiment_Polarity", col("Average_Sentiment_Polarity").cast(DoubleType)) //turns them as Double
+      .na.fill(0)// fills the NaN as 0.0
+
+    return df_1
+  }
+
+  /**
+   * To check if file exists
+   * @param pathToFile path of the file
+   * @param fileName file's name
+   * @param spark
+   * @return a Boolean:
+   * -> true - if file exists
+   * -> false - if file doesn't exist
+   */
+  def doesFileExists(pathToFile:String, fileName:String, spark:SparkSession): Boolean = {
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+    val fileExists = fs.exists(new Path(pathToFile+fileName))
+    return fileExists
+  }
+
+  /**
+   * writes a csv
+   * @param df dataframe wanted to be exported as csv
+   * @param delim delimiter
+   * @param pathToFile path of the file
+   */
+  def writeCSV(df: DataFrame, delim:String, pathToFile:String): Unit = {
+    df.coalesce(1).write
+      .option("delimiter", delim)
+      .csv(pathToFile)
+  }
+
+  /**
+   * renames a file
+   * @param pathToFile path of the file
+   * @param fileName wanted name to be written
+   * @param spark
+   */
+  def renameWrittenFile(pathToFile:String, fileName:String, spark:SparkSession): Unit = {
+
+    val ogName:String = "part*"
+
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+
+    val file = fs.globStatus(new Path(pathToFile+ogName))(0).getPath().getName()
+    //renames that file to best_apps.csv
+    fs.rename(new Path(pathToFile + file), new Path(pathToFile + fileName))
+    //delete temp file
+    fs.delete(new Path(fileName+"-temp"), true)
+  }
+
+  //Part 2
+  def partTwo(df:DataFrame, delim:String, pathToFile:String, fileName:String, spark:SparkSession): DataFrame = {
+
+    val ogName:String = "part*"
+
+    var df_2 = df
+      .withColumn("Rating", df("Rating").cast(DoubleType))
+      .filter(df("Rating") >= 4.0 && !isnan(df("Rating")))
+      .sort(desc("Rating"))
+
+    //check if file exists
+    val fileExists = doesFileExists(pathToFile, ogName, spark)
+
+    if (!fileExists) {
+      //writes the csv
+      writeCSV(df_2, delim, pathToFile)
+      //renames the csv
+      renameWrittenFile(pathToFile, fileName, spark)
+    }
+
+    return df_2
   }
 
 }
